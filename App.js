d3a36869a2bd8f46a64c5aaef3b8fc402421b060
Jhonathan Vitor <jhonathanvitor@gmail.com>
2020-04-19 02:37:20 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,19 +1,129 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+
+import {
+  Keyboard,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  AsyncStorage
+} from "react-native";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+StatusBar
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  async function addTask() {
+    const search = task.filter(task => task === newTask);
+
+    if (search.length !== 0) {
+      Alert.alert("Atenção", "Nome da tarefa repetido!");
+      return;
+    }
+
+    setTask([...task, newTask]);
+    setNewTask("");
+
+    Keyboard.dismiss();
+  }
+
+  async function removeTask(item) {
+    Alert.alert(
+      "Deletar Task",
+      "Tem certeza que deseja remover esta anotação?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return;
+          },
+          style: "cancel"
+        },
+        {
+          text: "OK",
+          onPress: () => setTask(task.filter(tasks => tasks !== item))
+        }
+      ],
+      { cancelable: false }
+    );
+  }
+
+  useEffect(() => {
+    async function carregaDados() {
+      const task = await AsyncStorage.getItem("task");
+
+      if (task) {
+        setTask(JSON.parse(task));
+      }
+    }
+    carregaDados();
+  }, []);
+
+  useEffect(() => {
+    async function salvaDados() {
+      AsyncStorage.setItem("task", JSON.stringify(task));
+    }
+    salvaDados();
+  }, [task]);
+
   return (
     <>
-      <View style={styles.container}>
-        <View style={styles.Body}>
-          <Text>Hello World!</Text>
-        </View>
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={0}
+        behavior="padding"
+        style={{ flex: 1 }}
+        enabled={Platform.OS === "ios"}
+      >
+        <StatusBar barStyle="light-content" backgroundColor="#8605B9" />
+          <View style={styles.container}>
+            <View style={styles.Body}>
+              <Text style={styles.ColorText}>Jhonathan Vitor</Text>
+              <FlatList
+                data={task}
+                keyExtractor={item => item.toString()}
+                showsVerticalScrollIndicator={false}
+                style={styles.FlatList}
+                renderItem={({ item }) => (
+                  <View style={styles.ContainerView}>
+                    <Text style={styles.Texto}>{item}</Text>
+                    <TouchableOpacity onPress={() => removeTask(item)}>
+                      <MaterialIcons
+                        name="delete-forever"
+                        size={25}
+                        color="#f64c75"
+                      />
+                    </TouchableOpacity>
+                  </View>
+                )}
+              />
+            </View>
+
           <View style={styles.Form}>
-            <TextInput />
-            <TouchableOpacity />
+            <TextInput
+              style={styles.Input}
+              placeholderTextColor="#999"
+              autoCorrect={true}
+              value={newTask}
+              placeholder="Adicione uma tarefa"
+              maxLength={25}
+              onChangeText={text => setNewTask(text)}
+            />
+            <TouchableOpacity style={styles.Button} onPress={() => addTask()}>
+              <Ionicons name="ios-add" size={20} color="white" />
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
-     
+      </KeyboardAvoidingView>
     </>
   );
 }
@@ -21,13 +131,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     paddingHorizontal: 20,
     paddingVertical: 20,
-    marginTop: 20
+    marginTop: 0,
+    backgroundColor: "#8605B9"
   },
   Body: {
     flex: 1
+  },
+  ColorText: {
+      color: "white",
+      fontSize: 18,
+      fontWeight: "bold"
   },
   Form: {
     padding: 0,
@@ -37,7 +152,48 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingTop: 13,
     borderTopWidth: 1,
-    borderColor: "#eee",
-    backgroundColor: "#777"
+    borderColor: "#eee"
+  },
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: "#eee",
+    borderRadius: 4,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#eee"
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1c6cce",
+    borderRadius: 4,
+    marginLeft: 10
+  },
+  FlatList: {
+    flex: 1,
+    marginTop: 5
+  },
+  Texto: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginTop: 4,
+    textAlign: "center"
+  },
+  ContainerView: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "#eee",
+
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 1,
+    borderColor: "#eee"
   }
 });
